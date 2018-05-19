1498b894a4387cf90698fcbd39bcaa08460cc9d2
lucas <lucas@akayama.com.br>
2018-05-19 13:19:06 -0400

    update arq

--- cnn.py
+++ cnn.py
@@ -19,38 +19,39 @@ if __name__ == '__main__':
     # Create the model
     model = Sequential()
 
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu', input_shape=(32, 32, 3)))
+    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(128, kernel_size=(3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
     model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(512, activation='selu', kernel_initializer='lecun_uniform'))
+    model.add(Dense(512, activation='selu', kernel_initializer='lecun_uniform'))
     model.add(Dropout(0.5))
     model.add(Dense(10, activation='softmax'))
 
-    opt = Adam(lr=0.0001, decay=1e-6)
+    opt = Adam(lr=0.0001, decay=2e-6)
     # Compile the model
     model.compile(loss='categorical_crossentropy',
                   optimizer=opt,
                   metrics=['accuracy'])
 
     # Train the model
-    model.fit(X_train / 255.0, to_categorical(Y_train),
+    model.fit(X_train, to_categorical(Y_train),
               batch_size=128,
               shuffle=True,
               epochs=250,
-              validation_data=(X_test / 255.0, to_categorical(Y_test)),
+              validation_data=(X_test, to_categorical(Y_test)),
               callbacks=[EarlyStopping(min_delta=0.001, patience=3)])
 
     # Evaluate the model
-    scores = model.evaluate(X_test / 255.0, to_categorical(Y_test))
+    scores = model.evaluate(X_test, to_categorical(Y_test))
 
     print('Loss: %.3f' % scores[0])
 print('Accuracy: %.3f' % scores[1])
